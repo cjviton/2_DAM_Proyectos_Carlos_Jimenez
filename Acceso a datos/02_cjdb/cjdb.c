c9313b9a9c06c8e758f942d2bb7425820828fd91
CarlosJV <cjimenezv334@alumnos.imf.com>
2023-10-21 18:54:41 +0200

    Guardar datos en archivo

--- Acceso a datos/02_cjdb/cjdb.c
+++ Acceso a datos/02_cjdb/cjdb.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    printf("JVDB");
-    
+    FILE *archivo;                    //creo un archivo de tipo file y lo llamo archivo
+    archivo = fopen("basededatos.txt","w"); //archivo es igual a fopen en modo writhe(escritura)
+    char *texto ="JVDB";
+    fputs(texto,archivo);
+    fclose(archivo);                     //cierro el archivo que he abierto con fopen
+        
 return 0;    
 }       
