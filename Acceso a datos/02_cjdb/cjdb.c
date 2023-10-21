be0478b1031244fa793513ebae71d533453ebc7d
CarlosJV <cjimenezv334@alumnos.imf.com>
2023-10-21 22:00:17 +0200

    Instrucciones Select e Insert
    Puedo elegir introducir datos en la base de datos o leer (select) de la base de datos

--- Acceso a datos/02_cjdb/cjdb.c
+++ Acceso a datos/02_cjdb/cjdb.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-/*De la función CRUD de la base de datos ya tendríamos hecho el create, ahora vamos a hacer el read
+/*De la función CRUD de la base de datos ya tendríamos hecho el create, ahora vamos a hacer el read o select
 operación = argv[1]
 basedatos = argv[2]
 nombrearchivo = argv[3]
 texto = argv[4]
+
+Implementamos en el codigo poder sacar datos por pantalla
 
 */
 
@@ -13,32 +15,43 @@ int main(int argc,char *argv[]){
     
     FILE *archivo;
     char *operacion = argv[1];
+    char *basededatos = argv[2];
+    char *nombrearchivo = argv[3];
     
-    if(strcmp(operacion,"select") == 0){  //si en el momento de la ejecucion introduzco select ejecutara ese código
+    char ruta[100];
+    strcpy(ruta,basededatos);
+    strcat(ruta,"-");
+    strcat(ruta,nombrearchivo);
+    strcat(ruta,".txt");
+    
+    if(strcmp(operacion,"select") == 0){
         printf("te doy datos");
-    }else if(strcmp(operacion,"insert") == 0){
-        char *basededatos = argv[2];;      //si en el momento de la ejecucion introduzco insert ejecutara ese código    
-        char *nombrearchivo = argv[3];    //Esta sección insert es todo el código del commit ante
-
-        char ruta[100];
-        strcpy(ruta,basededatos);
-        strcat(ruta,"-");
-        strcat(ruta,nombrearchivo);
-        strcat(ruta,".txt");
-
-        archivo = fopen(ruta,"a");
+        archivo = fopen(ruta,"r");         //como lo que quiero es leer, abro el archivo en modo read
+        printf("te doy datos:\n");
+        char linea[1024];
+        while(fgets(linea,sizeof(linea),archivo) != NULL){
+            printf("Linea: %s",linea);
+        }
+        
+     }else if(strcmp(operacion,"insert") == 0){
+        archivo = fopen(ruta,"a");        //como lo que quiero es añadir, abro el archivo en modo a
         char *texto = argv[4];
         fputs(strcat(texto,"\n"),archivo);
         fclose(archivo);
     }else{
         printf("operación no válida");
     }
-    return 0;
-        
+    
+    return 0;        
 }       
 
 /*
-La nstrucción sería esta:
-C:\Users\usuario\Documents\2ºDAM Proyectos Carlos Jimenez\Acceso a datos\02_cjdb>cjdb.exe "select"
+La nstrucción para inasertar datos sería esta: 
+
+C:\Users\usuario\Documents\2ºDAM Proyectos Carlos Jimenez\Acceso a datos\02_cjdb>cjdb.exe "insert" "Carlos" "clientes" "Jose Lopez"
+
+La nstrucción para hacer un select de datos sería esta: 
+
+C:\Users\usuario\Documents\2ºDAM Proyectos Carlos Jimenez\Acceso a datos\02_cjdb>cjdb.exe "select" "Carlos" "clientes"
 
 */
