dbeb0ffb9aa84557b8c9912f1677c500cb6b219b
CarlosJV <cjimenezv334@alumnos.imf.com>
2023-10-21 21:21:45 +0200

    Crear base de datos, tabla y datos
    Introduzco una variable más que será el nombre de la base de datos

--- Acceso a datos/02_cjdb/cjdb.c
+++ Acceso a datos/02_cjdb/cjdb.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
-/*Dentro de cjdb creo una carpeta, db. y vamos a supuner que de momento trabajo con una sola base de datos.
-Le voy a decir que el primer valor que le pasaré será el nombre de la tabla y el segundo nombre que le pasaré es lo que quiero guardar
+/*Ahora quiero seleccionar bases de datos entonces:
+basededatos=argv[1]
+nombredelarchivo=argv[2]
+texto=argv[3]
 */
 
 int main(int argc,char *argv[]){  
     
     FILE *archivo;
-    char *nombrearchivo = argv[1];                     //El nombre delarchivo es igual al arrg1 y el texto que me                                                        //guardará en ese archivo al arg2
-    archivo = fopen(strcat(nombrearchivo,".txt"),"a");
-    char *texto = argv[2];
+    char *basededatos = argv[1];  
+    char *nombrearchivo = argv[2];
+    
+    char ruta[100];
+    strcpy(ruta,basededatos);
+    strcat(ruta,"-");
+    strcat(ruta,nombrearchivo);
+    strcat(ruta,".txt");
+    
+    archivo = fopen(ruta,"a");    
+    char *texto = argv[3];
     fputs(strcat(texto,"\n"),archivo);
     fclose(archivo);
     return 0;
@@ -18,6 +28,7 @@ int main(int argc,char *argv[]){
 }       
 
 /*la orden en la consola de comandos seria la siguente:
-C:\Users\usuario\Documents\2ºDAM Proyectos Carlos Jimenez\Acceso a datos\02_cjdb>cjdb.exe "clientes" "Carlos J"
-De esta manera me está creando una carpeta en el directorio, con el nombre clientes y dentro el texto, CarlosJ
+C:\Users\usuario\Documents\2ºDAM Proyectos Carlos Jimenez\Acceso a datos\02_cjdb>cjdb.exe "Carlos J" "clientes" "José Peréz"
+
+De esta manera me está creando una carpeta en el directorio, con el nombre de la base de datos junto con el nombre de la tabla  y dentro el texto, José Pérez"
 */
