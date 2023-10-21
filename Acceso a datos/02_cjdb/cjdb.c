f84a95ab7bdbe34e23a50680198867f14ba5eaaf
CarlosJV <cjimenezv334@alumnos.imf.com>
2023-10-21 21:38:52 +0200

    Elegir Select o insert
    Al ejecutar le indico si quiero un hacer select o inset.
    Todo con la estructura if/else

--- Acceso a datos/02_cjdb/cjdb.c
+++ Acceso a datos/02_cjdb/cjdb.c
@@ -1,34 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
-/*Ahora quiero seleccionar bases de datos entonces:
-basededatos=argv[1]
-nombredelarchivo=argv[2]
-texto=argv[3]
+/*De la función CRUD de la base de datos ya tendríamos hecho el create, ahora vamos a hacer el read
+operación = argv[1]
+basedatos = argv[2]
+nombrearchivo = argv[3]
+texto = argv[4]
+
 */
 
 int main(int argc,char *argv[]){  
     
     FILE *archivo;
-    char *basededatos = argv[1];  
-    char *nombrearchivo = argv[2];
+    char *operacion = argv[1];
     
-    char ruta[100];
-    strcpy(ruta,basededatos);
-    strcat(ruta,"-");
-    strcat(ruta,nombrearchivo);
-    strcat(ruta,".txt");
-    
-    archivo = fopen(ruta,"a");    
-    char *texto = argv[3];
-    fputs(strcat(texto,"\n"),archivo);
-    fclose(archivo);
+    if(strcmp(operacion,"select") == 0){  //si en el momento de la ejecucion introduzco select ejecutara ese código
+        printf("te doy datos");
+    }else if(strcmp(operacion,"insert") == 0){
+        char *basededatos = argv[2];;      //si en el momento de la ejecucion introduzco insert ejecutara ese código    
+        char *nombrearchivo = argv[3];    //Esta sección insert es todo el código del commit ante
+
+        char ruta[100];
+        strcpy(ruta,basededatos);
+        strcat(ruta,"-");
+        strcat(ruta,nombrearchivo);
+        strcat(ruta,".txt");
+
+        archivo = fopen(ruta,"a");
+        char *texto = argv[4];
+        fputs(strcat(texto,"\n"),archivo);
+        fclose(archivo);
+    }else{
+        printf("operación no válida");
+    }
     return 0;
         
 }       
 
-/*la orden en la consola de comandos seria la siguente:
-C:\Users\usuario\Documents\2ºDAM Proyectos Carlos Jimenez\Acceso a datos\02_cjdb>cjdb.exe "Carlos J" "clientes" "José Peréz"
+/*
+La nstrucción sería esta:
+C:\Users\usuario\Documents\2ºDAM Proyectos Carlos Jimenez\Acceso a datos\02_cjdb>cjdb.exe "select"
 
-De esta manera me está creando una carpeta en el directorio, con el nombre de la base de datos junto con el nombre de la tabla  y dentro el texto, José Pérez"
 */
