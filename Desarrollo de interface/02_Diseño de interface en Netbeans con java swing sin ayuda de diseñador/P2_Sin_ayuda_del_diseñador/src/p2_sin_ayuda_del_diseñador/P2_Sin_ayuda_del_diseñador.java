587de62f98f1b34739bccec969a791fbc63fe353
CarlosJV <cjimenezv334@alumnos.imf.com>
2023-10-22 13:04:27 +0200

    Primeros pasos

--- Desarrollo de interface/02_Diseño de interface en Netbeans con java swing sin ayuda de diseñador/P2_Sin_ayuda_del_diseñador/src/p2_sin_ayuda_del_diseñador/P2_Sin_ayuda_del_diseñador.java
+++ Desarrollo de interface/02_Diseño de interface en Netbeans con java swing sin ayuda de diseñador/P2_Sin_ayuda_del_diseñador/src/p2_sin_ayuda_del_diseñador/P2_Sin_ayuda_del_diseñador.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package p2_sin_ayuda_del_diseñador;
 
-/**
- *
- * @author Carlos
- */
-public class P2_Sin_ayuda_del_diseñador {
+public class P2_Sin_ayuda_del_diseñador extends javax.swing.JFrame {//1ºextent a la libreria de java    swing
+    
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
