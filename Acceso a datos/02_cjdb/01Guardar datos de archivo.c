64c8ab658d5946cf401c34e2287edb16f42f6bf6
CarlosJV <cjvdeveloper@gmail.com>
2023-10-28 19:42:41 +0200

    Pasos
    Implemento todos los pasos que he seguido hasta el código fianal

---
+++ Acceso a datos/02_cjdb/01Guardar datos de archivo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    FILE *archivo;                          //creo un archivo de tipo file y lo llamo archivo
+    archivo = fopen("basededatos.txt","w"); //archivo es igual a fopen en modo writhe(escritura)
+    char *texto ="JVDB";
+    fputs(texto,archivo);
+    fclose(archivo);                          //cierro el archivo que he abierto con fopen
+
+return 0;    
+} 
---
+++ Acceso a datos/02_cjdb/03Crear_guardar datos en tablas.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+Le voy a decir que el primer valor que le pasaré será el nombre de la tabla y el segundo nombre que le pasaré es lo que quiero guardar
+*/
+
+int main(int argc,char *argv[]){  
+    
+    FILE *archivo;
+    char *nombrearchivo = argv[1];                     //El nombre delarchivo es igual al arrg1 y el texto que me                                                                                                                    //guardará en ese archivo al arg2
+    archivo = fopen(strcat(nombrearchivo,".txt"),"a");
+    char *texto = argv[2];
+    fputs(strcat(texto,"\n"),archivo);
+    fclose(archivo);
+    return 0;
+        
+}       
+
+/*la orden en la consola de comandos seria la siguente:
+C:\Users\usuario\Documents\2ºDAM Proyectos Carlos Jimenez\Acceso a datos\02_cjdb>cjdb.exe "clientes" "Carlos J"
+De esta manera me está creando una carpeta en el directorio, con el nombre clientes y dentro el texto, CarlosJ
+*/
