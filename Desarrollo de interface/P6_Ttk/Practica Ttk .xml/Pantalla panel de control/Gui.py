6f6e0b69ca2baf73e124b7a8af574c479b9740fa
CarlosJV <cjvdeveloper@gmail.com>
2023-12-02 19:57:20 +0100

    Desarrollo python terminado
    El desarrollo de python está terminado. Falta modificar el archivo XML para dar forma al panel de control

--- Desarrollo de interface/P6_Ttk/Practica Ttk .xml/Pantalla panel de control/Gui.py
+++ Desarrollo de interface/P6_Ttk/Practica Ttk .xml/Pantalla panel de control/Gui.py
@@ -6,13 +6,60 @@ from bs4 import BeautifulSoup
 import tkinter as tk
 
 raiz = tk.Tk()
+raiz.geometry("800x600+10+10")  #Cambio la geometría de la ventaja. 
+raiz.title ("Penel de control") #Pongo título en la vntana
+raiz.iconbitmap("logo.ico")     #Favicon windows
+
+
+#Menus de la ventana
+
+barramenu=tk.Menu(raiz)
+raiz.config(menu=barramenu)
+archivo = tk.Menu(barramenu,tearoff=0) 
+barramenu.add_cascade(label="Archivo",menu=archivo)
+
+archivo.add_command(label="Nuevo")
+archivo.add_command(label="Abrir")
+archivo.add_command(label="Guardar")
+archivo.add_command(label="Salir")
+
+
+editar = tk.Menu(barramenu,tearoff=0) 
+barramenu.add_cascade(label="Editar", menu=editar)
+
+editar.add_command(label="Deshacer")
+editar.add_command(label="Copiar")
+editar.add_command(label="Cortar")
+editar.add_command(label="Pegar")
+
+
+ayuda = tk.Menu(barramenu,tearoff=0) 
+barramenu.add_cascade(label="Ayuda",menu=ayuda)
+
+ayuda.add_command(label="Ayuda")
+ayuda.add_command(label="Documentacion en linea")
+ayuda.add_command(label="Acerda de")
+ayuda.add_command(label="Soporte")
+
 
 #Creo una ventana partida y lal meto en raiz con orientación
 ventanapartida = tk.PanedWindow(raiz,orient=tk.HORIZONTAL)
 
 #Creo 2 Frame y los llamo marco1 y marco2
-marco1 = tk.Frame (ventanapartida,background="grei")
-marco2 = tk.Frame (ventanapartida,background="blue")
+marco1 = tk.Frame (ventanapartida,background="grey")
+marco2 = tk.Frame (ventanapartida,background="dark grey")
+
+
+
+#Bucle for para dividir el marco2 en  en 3 columnas y 3 filas
+for i in range(3):                   
+    marco2.columnconfigure(i, weight=1)
+    
+# Bucle for para dividir el marco2 en 3 filas
+for i in range(3):
+    marco2.rowconfigure(i, weight=1)
+
+    
 
 #Los añado a ventana partida
 ventanapartida.add(marco1)
@@ -22,7 +69,9 @@ ventanapartida.add(marco2)
 ventanapartida.pack(fill=tk.BOTH,expand=True)
 
 
-archivo= open("controlador.xml","r") #Abro el archivo interfaz en modo lectura
+
+#Abro el archivo controlador en modo lectura
+archivo= open("controlador.xml","r") 
 contenido = archivo.read()
 xml=BeautifulSoup(contenido,"xml")
 
@@ -39,9 +88,10 @@ for marco in xml.find_all("marco"):  #Con el bucle for le digo que busque en el 
                 
             tipo = campo.get ("tipo")        #Capturo el tipo de campo del xml de marco 1
             texto = campo.get("texto")       #Capturo el campo texto del xml
-
+            
+            
             if tipo == "entrada":
-                tk.Entry(marco1).pack(padx=10,pady=10)
+                tk.Entry(marco1).pack(padx=10,pady=10)                
             elif tipo == "etiqueta":
                 tk.Label(marco1,text=texto).pack(padx=10,pady=10)
             elif tipo == "boton":
@@ -53,13 +103,20 @@ for marco in xml.find_all("marco"):  #Con el bucle for le digo que busque en el 
 
             tipo = campo.get ("tipo")        #Capturo el tipo de campo del xml de marco 2
             texto = campo.get("texto")       #Capturo el campo texto del xml
+            row = campo.get("row")           #Capturo el campo row
+            column = campo.get("column")     #Capturo el campo column
 
+            
             if tipo == "entrada":
-                tk.Entry(marco2).pack(padx=10,pady=10)
+                entrada1 = tk.Entry(marco2)
+                entrada1.grid(row=row,column=column,padx=10,pady=10)
             elif tipo == "etiqueta":
-                tk.Label(marco2,text=texto).pack(padx=10,pady=10)
+                entrada2 = tk.Label(marco2,text=texto)
+                entrada2.grid(row=row,column=column,padx=10,pady=10)
             elif tipo == "boton":
-                tk.Button(marco2,text=texto).pack(padx=10,pady=10)
+                entrada3 = tk.Button(marco2,text=texto)
+                entrada3.grid(row=row,column=column,padx=10,pady=10)
+                
 
 
 tk.mainloop()
