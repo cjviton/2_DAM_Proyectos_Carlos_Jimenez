eb721584354cd19a68038460779e2678363e6337
CarlosJV <cjvdeveloper@gmail.com>
2023-12-03 19:52:42 +0100

    Clase  cjdb funcionado

--- Acceso a datos/05_cjdb/conexion en clase.py
+++ Acceso a datos/05_cjdb/conexion en clase.py
@@ -3,19 +3,31 @@
 
 import subprocess
 
+#Creo una clase con un método constructor 
+class Cjdb:
+    def __init__(self,basededatos):
+        self.basededatos = basededatos
+
+#Creo un método donde voy a meter el código 
+
+    def insert(self,coleccion,documento,contenido):                #Convierto en propiedades del método las variables
+        self.operacion = "insert"
+        self.coleccion = coleccion
+        self.documento = documento
+        self.contenido = contenido
+        comando =  '"C:\\Users\\usuario\\Documents\\2ºDAM Proyectos Carlos Jimenez\\Acceso a datos\\05_cjdb\\cjdb.exe" '+self.operacion+' '+self.basededatos+' '+self.coleccion+' '+self.documento+' "'+self.contenido+'"'
+        resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+
+        if resultado.returncode == 0:
+            return("ok")
+        else:
+            return("ko")
 
 
-operacion = "insert"
-basededatos = "miempresa"
-coleccion = "clientes"
-documento = "cliente5"
-contenido = "Este es el cliente 5"
 
+#Creo un objeto de la clase Cjdb
+        
+Conexion1 = Cjdb("miEmpresa")
 
-comando =  '"C:\\Users\\usuario\\Documents\\2ºDAM Proyectos Carlos Jimenez\\Acceso a datos\\05_cjdb\\cjdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' "'+contenido+'"'
-resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
-
-if resultado.returncode == 0:
-    print("ok")
-else:
-    print("ko")
+#Llamo al metodo insert del objeto conexion1 de la clase Cjdb con sus parametros (coleccion,documento,contenido)
+Conexion1.insert("clientes","cliente6","Este es el contenido del cliente 6")
