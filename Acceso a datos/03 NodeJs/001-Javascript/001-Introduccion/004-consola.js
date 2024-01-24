b103f76879adba4eed78ad4476596dc222382b36
CarlosJV <cjvdeveloper@gmail.com>
2024-01-24 20:44:56 +0100

    Fundamentos de javascript
    Estructura de carpetas sin contenido

---
+++ Acceso a datos/03 NodeJs/001-Javascript/001-Introduccion/004-consola.js
@@ -0,0 +1,4 @@
+//Este archivo es javascript .js, no HTML como los anteriores. Es javascript puro.
+//Vamos a ejecutar los script de node en la consola de comandos.
+
+console.log("Hola mundo desde Javascript");
