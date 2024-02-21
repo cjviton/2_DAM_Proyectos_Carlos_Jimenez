ae22946c1ce930028f980257f6ce7c8b6341aacb
CarlosJV <cjvdeveloper@gmail.com>
2024-02-21 23:04:37 +0100

    web restaurante con node
    configuradas todas las pantallas a falta de hacer persistencia de datos con las página de reservas

--- Acceso a datos/03 NodeJs/004-MySQL/009-Restaurante/001-servidor.js
+++ Acceso a datos/03 NodeJs/004-MySQL/009-Restaurante/001-servidor.js
@@ -1,4 +1,3 @@
-
 //Cargo los módulos necesarios. Para que funcione mi web con la base de datos
 //tengo que cargar a parte de los que tenía, el módulo mysql
 
@@ -8,97 +7,79 @@ var ruta = require('url');
 var procesador = require('querystring');
 var mysql = require('mysql')
 
-
 //Hago la conexión con la base de datos.
 var conexion = mysql.createConnection({
-                host:"localhost",
-                user:"nodejs",
-                password:"nodejs",
-                database:"nodejs"
-            });
+    host: "localhost",
+    user: "nodejs",
+    password: "nodejs",
+    database: "nodejs"
+});
 
 //le pido confirmación de conexión por si hubiera algún error.
-conexion.connect(function(err){
-                if(err) throw err;
-                console.log("conectado")
- })    
-
+conexion.connect(function(err) {
+    if (err) throw err;
+    console.log("conectado")
+})
 
 //Creo el servidor que escuchará por el puerto 8090
-servidor.createServer(function(req,res){
-   res.writeHead(200,{'Content-Type':'text/html'})
+servidor.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
 
-   //variable para guardar en el archivo registro las fecha y hora de las 
-   //visitas a las diferentes páginas.
-   var rutacompleta = ruta.parse(req.url,true)
+    //variable para guardar en el archivo registro las fecha y hora de las 
+    //visitas a las diferentes páginas.
+    var rutacompleta = ruta.parse(req.url, true)
 
     //estructura de switch para cambiar entre las diferentes páginas.
-    archivos.readFile('plantillas/cabecera.html',function(err,data){
-        res.write(data)
-       switch(req.url){
+    switch (req.url) {
         case "/":
-            archivos.readFile('inicio.html',function(err,data){
-                res.write(data)
+            // Cuando la URL solicitada es "/", responder con el contenido de inicio.html
+            archivos.readFile('cabecera.html', function(err, data) {
+                if (err) {
+                    console.error('Error al leer inicio.html:', err);
+                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                    res.end('Error interno del servidor');
+                    return;
+                }
+                res.write(data);
+                res.end();
             });
             break;
-        case "/sobremi":
-            archivos.readFile('sobremi.html',function(err,data){
-                res.write(data)
+        case "/menu":
+            // Cuando la URL solicitada es "/sobremi", responder con el contenido de sobremi.html
+            archivos.readFile('menu.html', function(err, data) {
+                if (err) {
+                    console.error('Error al leer sobremi.html:', err);
+                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                    res.end('Error interno del servidor');
+                    return;
+                }
+                res.write(data);
+                res.end();
             });
             break;
-        case "/blog":  //Aquí es donde hago la peticion de select a la bbdd para que me lo 
-                      //muestre en mi consola.
-            conexion.query(`
-                SELECT * FROM entradas
-            `,function(err,result,fields){
-                if(err) throw err;
-                console.log(result)
-                for(let i = 0;i<result.length;i++){
-                    console.log(result[i])
-                    res.write(`
-                        <article>
-                            <h4>`+result[i].titulo+`</h4>
-                            <time>`+result[i].fecha+`</time>
-                            <p>`+result[i].texto+`</p>
-                        </article>
-                    `)
-                } 
-            })
-           
-            break;
-        case "/contacto":
-            archivos.readFile('contacto.html',function(err,data){
-                res.write(data)
-            });
-            break;
-        case "/procesa":
-            let datos = '';
-               req.on('data',parte=>{
-                   datos += parte.toString();
-               })
-               req.on('end',()=>{
-                   var cadena = datos
-                   var procesado = procesador.parse(cadena)
-                   console.log(procesado)
-               })
-            
-            break;
-        default:
-            res.end("Página no encontrada");
-    } 
-     archivos.readFile('plantillas/piedepagina.html',function(err,data){
-        res.write(data)
-         res.end("")
-    });
-    });
-   
+        
+            case "/reserva":
+                // Cuando la URL solicitada es "/sobremi", responder con el contenido de sobremi.html
+                archivos.readFile('reserva.html', function(err, data) {
+                    if (err) {
+                        console.error('Error al leer sobremi.html:', err);
+                        res.writeHead(500, { 'Content-Type': 'text/plain' });
+                        res.end('Error interno del servidor');
+                        return;
+                    }
+                    res.write(data);
+                    res.end();
+                });
+                break;
+        
+    }
 
     //Estructur para guardar los datos en el registro.
-    if(req.url != "/favicon.ico"){
-       var fecha = new Date();
-    archivos.appendFile("registro.txt",fecha.getFullYear()+","+fecha.getMonth()+","+fecha.getDate()+","+fecha.getHours()+","+fecha.getMinutes()+","+fecha.getSeconds()+","+rutacompleta.host+","+rutacompleta.pathname+","+rutacompleta.search+","+req.url+"\n",function(err){
-            if(err) throw err;
-        })
+    if (req.url != "/favicon.ico") {
+        var fecha = new Date();
+        archivos.appendFile("registro.txt", fecha.getFullYear() + "," + fecha.getMonth() + "," + fecha.getDate() + "," + fecha.getHours() + "," + fecha.getMinutes() + "," + fecha.getSeconds() + "," + rutacompleta.host + "," + rutacompleta.pathname + "," + rutacompleta.search + "," + req.url + "\n", function(err) {
+            if (err) throw err;
+        });
     }
-    
+
 }).listen(8090)
