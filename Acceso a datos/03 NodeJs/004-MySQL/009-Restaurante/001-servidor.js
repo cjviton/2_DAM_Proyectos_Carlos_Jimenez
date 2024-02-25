24628520ce2fb188e553b40737783b27bd7e02c4
CarlosJV <cjvdeveloper@gmail.com>
2024-02-25 19:09:37 +0100

    Con reservas  confirmadas escritas en reservasConfirmadas.html

--- Acceso a datos/03 NodeJs/004-MySQL/009-Restaurante/001-servidor.js
+++ Acceso a datos/03 NodeJs/004-MySQL/009-Restaurante/001-servidor.js
@@ -12,7 +12,7 @@ const conexion = 'mongodb://127.0.0.1/restaurante'
 
 //le pido confirmación de conexión por si hubiera algún error.
 mongoose.connect(conexion,{useNewUrlParser:true,useUnifiedTopology:true}).then(function(){
-    console.log("conectado a mongo")
+    console.log("conectado a mongoDB")
 })
 
 //CReo un objeto de la clase mongoose con la estructura que tengo en mi base de datos.
@@ -94,26 +94,25 @@ servidor.createServer(function(req, res) {
                     if (err) {
                         console.error('Error al leer reserva.html:', err);
                         res.writeHead(500, { 'Content-Type': 'text/plain' });
-                        res.end('Error interno del servidor');
+                        res.end('Error interno del servidor');7
                         return;
                     }
-
-
                     res.write(data);
                     res.end();
                 });
                 break;
-            
+            //Cuando puulse el botón enviar la url será /procesa y se ejecutará este código. 
             case "/procesa":
-                let datos = '';
-               req.on('data',parte=>{
+                let datos = '';//decalro la variable para declara la cadena vacía datos.
+               req.on('data',parte=>{ //evento require para escucahr los datos que llegan en la solicitud
                    datos += parte.toString();
                })
                req.on('end',()=>{
                 var cadena = datos
-                var procesado = procesador.parse(cadena)
+                var procesado = procesador.parse(cadena)//Se parsean los datos, convierte la cadena recibida en 
+                                                        //en un objeto javascript
                 console.log(procesado)
-                var nnombre = procesado.nombre
+                var nnombre = procesado.nombre  //los datos procesaados se asignan se asignan a variables 
                 var ndia = procesado.dia
                 var nmes = procesado.mes
                 var nhora = procesado.hora
@@ -121,7 +120,7 @@ servidor.createServer(function(req, res) {
                 var ncochinillo = procesado.cochinillo
                 var ninfantil = procesado.infantil
                 
-                var NuevaReserva = new Reserva({
+                var NuevaReserva = new Reserva({  //Creo una instancia nuevaReserva del objeto Reserva
                      nombre:nnombre,
                      dia:ndia,
                      mes:nmes,
@@ -131,13 +130,25 @@ servidor.createServer(function(req, res) {
                      infantil:ninfantil
                  })
                 
-                NuevaReserva.save()
-                 .then(function(){
+                NuevaReserva.save()   //Llamo el método save de la instancia reserva para guardar el documento
+                 .then(function(){    //en la base de datos.
                      console.log("Insertado")
+                     archivos.readFile('reserva.html', function(err, data) {//Caundo salve en la bbdd recarga la página /Reserva
+                        if (err) {
+                            console.error('Error al leer reserva.html:', err);
+                            res.writeHead(500, { 'Content-Type': 'text/plain' });
+                            res.end('Error interno del servidor');
+                            return;
+                        }
+                        res.write(data);
+                        res.end();
+                    });
                  })
                 
             })
+            break;
 
+            
             case "/95147":
             // Cuando la URL solicitada es "/95147", responder con el contenido de reservasConfirmadas.html
             archivos.readFile('reservasConfirmadas.html', function(err, data) {
@@ -147,13 +158,35 @@ servidor.createServer(function(req, res) {
                     res.end('Error interno del servidor');
                     return;
                 }
-                
-                res.write(data);
-                res.end();
+                Reserva.find({}).exec()
+                    .then(function(reservas) {
+                        console.log(reservas); // Imprimir en consola los resultados
+
+                        // Escribir los resultados en la respuesta HTML
+                        res.write('<h1>RESERVAS CONFIRMADAS PARA HOY</h1>');
+                        reservas.forEach(function(reserva) {
+                            res.write('<h2>Nombre:  ' + reserva.nombre + '</h2>');
+                            res.write('<p>Dia:  ' + reserva.dia + '</p>');
+                            res.write('<p>Mes:  ' + reserva.mes + '</p>');
+                            res.write('<p>Hora:  ' + reserva.hora + '</p>');
+                            res.write('<p>Menu gastonomico:  ' + reserva.gastronomico + '</p>');
+                            res.write('<p>Cochinillo:  ' + reserva.cochinillo + '</p>');
+                            res.write('<p>Menu infantil:  ' + reserva.infantil + '</p>');
+                        });
+                        res.write('</reservas_para_ho>');
+
+                        res.end(); // Finalizar la respuesta después de escribir los datos
+                    })
+                    .catch(function(err) {
+                        console.error('Error al leer reservas de la base de datos:', err);
+                        res.writeHead(500, { 'Content-Type': 'text/plain' });
+                        res.end('Error al leer reservas de la base de datos');
+                    });
+            
             });
             break;
     }
-/*
+
     //Estructur para guardar los datos en el registro.
     if (req.url != "/favicon.ico") {
         var fecha = new Date();
@@ -161,5 +194,5 @@ servidor.createServer(function(req, res) {
             if (err) throw err;
         });
     }
-*/
+
 }).listen(8090)
