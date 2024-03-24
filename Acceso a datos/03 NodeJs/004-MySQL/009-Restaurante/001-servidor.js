e54d970422e3616811660b778ab6d40868fac919
= <=>
2024-03-24 19:36:07 +0100

    Guarda los datos del formulario en la base de datos en json

--- Acceso a datos/03 NodeJs/004-MySQL/009-Restaurante/001-servidor.js
+++ Acceso a datos/03 NodeJs/004-MySQL/009-Restaurante/001-servidor.js
@@ -1,21 +1,75 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const mongoose = require('mongoose');
+const querystring = require('querystring'); 
+
+
+
+// Conexión a MongoDB
+const connectionString = 'mongodb://localhost:27017/restaurante'; 
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Conectado a MongoDB'))
+    .catch(err => console.error('Error de conexión a MongoDB:', err));
+
+// Definición de esquema y modelo de Mongoose
+const reservaSchema = new mongoose.Schema({
+    nombre: String,
+    dia: String,
+    mes: String,
+    hora: String,
+    gastronomico: String,
+    cochinillo: String,
+    infantil: String
+});
+
+const Reserva = mongoose.model('Reserva', reservaSchema, 'reservas');
+
+
+
 
 const server = http.createServer((req, res) => {
     let filePath = '.' + req.url;
     // Furzo el root a cabecera.htm, y el resto las cargo directas
     if (filePath === './') {
         filePath = './public/cabecera.html';
-    }
-    else if(filePath === './menu'){
+
+    }else if(filePath === './menu'){
         //DDentro de este bloque de aqui tu puedes poner lo que hagga falta con mongoose o con lo que quieras ///////////////////////////////
         filePath = './public/menu.html';
         
-    }
-    else if(filePath === './reserva'){
+    }else if(filePath === './reserva'){
         //DDentro de este bloque de aqui tu puedes poner lo que hagga falta con mongoose o con lo que quieras ///////////////////////////////
         filePath = './public/reserva.html';
+        
+    }else if(filePath === './procesa'){
+        let datos = '';
+        req.on('data', parte => {
+            datos += parte.toString();
+        });
+        req.on('end', () => {
+            const procesado = querystring.parse(datos);
+            const nuevaReserva = new Reserva({
+                nombre: procesado.nombre,
+                dia: procesado.dia,
+                mes: procesado.mes,
+                hora: procesado.hora,
+                gastronomico: procesado.gastronomico,
+                cochinillo: procesado.cochinillo,
+                infantil: procesado.infantil
+            });
+            nuevaReserva.save()
+                
+                .then(() => {
+                    console.log('Reserva guardada en MongoDB');
+                    // Responder al cliente como lo haces en otros casos
+                    
+                })
+                .catch(error => {
+                    console.error('Error al guardar reserva en MongoDB:', error);
+                    // Responder al cliente con un mensaje de error apropiado
+                });
+        });
         
     }
     else if(filePath === './12345'){
